6d35e0b1db2f2dbb67b05bfb77b292fb741581fc
sergio <sergiolinkpf@yahoo.com.mx>
2013-06-12 23:51:27 -0800

    +Update jQuery for 1.7 to 1.10 +Update navigator

---
+++ client/AJAXpages/vueloRecorrido.php
@@ -0,0 +1,22 @@
+<div id="mainContainer" class="container-fluid">
+    <div class="row-fluid">
+        <div class="span6 hero-unit rightline">
+            <h1>Instructions/Tutorial</h1>
+            <p>See a tutorial about how control the device.</p>
+            <div class="middle">
+                <p>
+                    <a class="btn btn-large btn-primary">Learn</a>
+                </p>
+            </div>
+        </div>
+        <div class="span6 hero-unit">
+            <h1>Start travel</h1>
+            <p>Click the below button to start the travel</p>
+            <div class="middle">
+                <p>
+                    <a class="btn btn-large btn-primary">Start</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
--- client/js/indexcliente.js
+++ client/js/indexcliente.js
@@ -4,4 +4,6 @@ $(document).on("ready",function(){
 		cache: false
 	});
 	var ajax_load = "<img src='images/load.gif' alt='Loading...'>";
+	var loadUrlIni = "AJAXpages/index_inicio.php";
+	var loadUrlAddUsr = "AJAXpages/admon_user_add.php";
 });
