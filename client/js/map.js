951e726189f44ca4553c44124fad655d917683d5
sergio <sergiolinkpf@yahoo.com.mx>
2013-07-10 14:07:07 -0800

    +Update Map.js - Now is posible create N markers in Google Map and mofificate position each one.

--- client/js/map.js
+++ client/js/map.js
@@ -7,45 +7,96 @@
 
 //La tecnica utilizada será declarar un objeto (variable, clase,namespace, etc) global y meter todas mis variable alli
 // dentro. El objetivo e sno contaminar el espacio global con mis variables.
-
-
-var coorMap = new google.maps.LatLng(-4.397, 120.644);
-var coorMarker = new google.maps.LatLng(-4.4, 120.644);
-
-function initialize() {
-    var mapOptions = {
-      center: coorMap,				//Centro del mapa de Google.
-      zoom: 10,
+function initialize(amountParticles) {
+	
+	// Returns a random integer between min and max
+	// Using Math.round() will give you a non-uniform distribution!
+	function getRandomInt(min, max) {
+	  return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	
+	var Particle = function(){
+		var id = "id= "+getRandomInt(1,100);						//Identificador de la particula
+		
+		var coorMarker; 
+		// To add the marker to the map, call setMap();
+		var marker = new google.maps.Marker({
+		    	position: new google.maps.LatLng(0,0),						//Posicion inicial
+		    	title:"Particle: "+id,
+		    	map: map
+		});
+		//marker.setMap(map);			//map es heredado.
+		
+		// Get position of this particle.
+		this.step = function(){
+			// Obtener su posicion, las opciones son eventos o indexado.
+			// Eventos: No necesario porque la transmision de datos es casi constante, si fueran el lapsos de tiempo aleatorio entonces si convendira.
+			// Indezado: Se localiza atraves de un nombre unica del elemento.
+			coorMarker = new google.maps.LatLng(getRandomInt(-90,90),getRandomInt(-180,180));
+		}
+		
+		// Draw particle
+		this.draw = function(){
+			//Notice that you cannot modify the coordinates of a LatLng.
+			//If you want to compute another point, you have to create a new one.
+			//https://developers.google.com/maps/documentation/javascript/reference#LatLng
+			marker.setPosition(coorMarker);
+			console.log("Cambio de "+id);
+		}
+	}
+	
+	
+	var mapCanvas = document.getElementById("map-canvas");
+	//Create map
+	var mapOptions = {									// Al estar dentro de la funcion initialize se comporta como una variable privada.
+      center: new google.maps.LatLng(0,0),				//Centro del mapa de Google.
+      zoom:2,
       mapTypeId: google.maps.MapTypeId.TERRAIN
     };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),
-        mapOptions);
-        
-	var marker = new google.maps.Marker({
-    	position: coorMarker,
-    	title:"Hello World!"
-	});
-
-	// To add the marker to the map, call setMap();
-	marker.setMap(map);
+    var map = new google.maps.Map(mapCanvas,mapOptions);
+    
+    Particle.prototype.map = map;			//Todos los objetos van a heredar a mapa.    
 	
-	//setInterval, setInterval ->http://www.w3schools.com/js/js_timing.asp
+	// Create all particles:
+	var particles = [];
+	for(var i=0; i<amountParticles;i++){
+		particles.push(new Particle());
+	}
 	
+	// The setInterval method accepts a function as its first parameter and the delay in milliseconds in which to repeatedly call this function.
+	//setInterval ->http://www.w3schools.com/js/js_timing.asp
+	/*
 	setInterval(function(){
-		//alert("cambio");
-		coorMarker['jb'] = coorMarker['jb'] + 0.1;
-		console.log(coorMarker['jb']);
-		}, 1000);
-	//coorMarker['jb'] = -4.5;
+		
+		for(var i=0 ; i<amountParticles; i++){
+            var a = particles[i];
+		
+			//Then the particle is stepped and drawn
+	        a.step();
+	        a.draw();
+   		}
+		// Stop ejecucion
+		//var myVar = setInterval(function(){myTimer()},1000);
+		//function myTimer() {
+		//	var d=new Date();
+		//	var t=d.toLocaleTimeString();
+		//	document.getElementById("demo").innerHTML=t;
+		//} 
+		//window.clearInterval(intervalVariable)
+	},200);
+	*/
 	
-	// Stop ejecucion
-	//var myVar = setInterval(function(){myTimer()},1000);
-	//function myTimer() {
-	//	var d=new Date();
-	//	var t=d.toLocaleTimeString();
-	//	document.getElementById("demo").innerHTML=t;
-	//} 
-	//window.clearInterval(intervalVariable)
+	//Self Invoking function. Preferible uzarlo en sustitucion a setInterval().
+	!function runTracking(){
+		for(var i=0 ; i<amountParticles; i++){
+            var a = particles[i];
+		
+			//Then the particle is stepped and drawn
+	        a.step();
+	        a.draw();
+   		}
+   		setTimeout(runTracking,200)
+	}();
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize(40));
